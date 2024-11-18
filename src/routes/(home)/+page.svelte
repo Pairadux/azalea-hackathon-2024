ccd6ba5dfdaa15e395740751dc675285ff13b4a5
Pairadux <austincgause@gmail.com>
2024-11-18 11:17:39 -0500

    update

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -11,12 +11,4 @@
 
 <section class="flex flex-[0.6] flex-col items-center justify-center">
 	<p class="text-4xl font-black text-black dark:text-white">Valdosta Medical</p>
-	<p class="mt-2 text-xl text-black dark:text-white">Welcome to Valdosta Medical. Please sign in using your username and password.</p>
-	<p class="mt-2 text-xl text-black dark:text-white">For your convenience, here are some starter accounts</p>
-	<ul class="mt-6 text-xl text-black dark:text-white">
-		<li>Pepper: password123</li>
-		<li>Joker: mypassword</li>
-		<li>Bob: securepass</li>
-		<li>Jeff: anotherpass</li>
-	</ul>
 </section>
